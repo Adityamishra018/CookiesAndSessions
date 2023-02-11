7fea5b5bf85a34d9262a42a8d6ce088c122e4f5c
Aditya Mishra <adi.adityamishra007@gmail.com>
2023-02-11 23:21:26 +0530

    express-session example added

---
+++ session.js
@@ -0,0 +1,79 @@
+import express from "express"
+import session from "express-session"
+import bodyParser from "body-parser"
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(session({
+    secret : "MagicStrinG",
+    saveUninitialized: false,
+    resave : false,
+    rolling : true,
+    cookie : {
+        maxAge : 1000*60*1,
+        httpOnly: true,
+        secure : false
+    }
+}))
+
+function isAuthenticated(req,res,next){
+    if(req.session.user)
+        next()
+    else
+        res.redirect("/")
+}
+
+app.get("/",(req,res)=>{
+    res.send(`
+    <form method="post" action="/">
+        Username : <input name="username" type="text" /></br>
+        Password : <input name="password" type="password" /></br>
+        <button>Submit</button>
+    </form>
+    `);
+})
+
+app.post("/",(req,res,next)=>{
+    //generate session for this user (using cookies internally)
+    req.session.regenerate((err)=>{
+        req.session.user = req.body.username
+    })
+
+    //save session in session-store, similar to how you'd store in any db
+    //since by default this is in memory store, if you restart server the session info will be lost
+    req.session.save((err)=>{
+        if(err)
+            next(err)
+        
+        res.redirect("/homepage");
+    })
+})
+
+app.get("/homepage",isAuthenticated,(req,res)=>{
+    res.send(`
+        <h1>Hello ${req.session.user}</h1>
+        <h2>Session lasts for 1 min in rolling fashion</h2>
+        <a href="/logout">End Session Now</a>
+    `)
+})
+
+
+app.get("/logout",(req,res)=>{
+    req.session.user = null
+    //save changes on store before you leave the page
+    req.session.save((err)=>{
+        if(err)
+            next(err)
+        
+        //send empty new session 
+        req.session.regenerate((err)=>{
+            if(err)
+                next(err)
+            res.redirect("/")
+        })
+    })
+})
+
+app.listen(3000)
+
